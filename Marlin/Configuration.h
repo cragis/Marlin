d9d4f9f3848aa128aabb9379d1a3ac2bbb55847a
cragis <craighowald@hotmail.com>
2022-04-11 16:48:54 -0400

    McP-22b with skr1.3 instead of skr1.4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Craig Howald, 20220201 McP SKR V1.4 Bear TMC2209 BLTouch v22a)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Craig Howald, 20220201 McP SKR V1.3 Bear TMC2209 BLTouch v22b)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -139,7 +139,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V1_4
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_3
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
