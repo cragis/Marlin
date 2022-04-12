6a3b532741ca8f56db61bfe881c314962c591dbd
cragis <craighowald@hotmail.com>
2022-04-12 09:29:58 -0400

    E-steps corrected to 140

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -915,7 +915,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 280 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 140 }
 
 /**
  * Default Max Feed Rate (mm/s)
